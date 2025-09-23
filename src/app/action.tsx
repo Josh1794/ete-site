34f9ddf5c06aa5b3d0fd773e85f2eb3d25601755
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-09-23 15:19:29 -0400

    Update action.tsx

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server';
 import { Resend } from 'resend';
-import { toast } from 'sonner';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function handler(formData: FormData) {
